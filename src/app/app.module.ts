170431869f00fa5da061cca8ec17cf8c0ba577f2
Eric Verdun <eric.verdun@atos.net>
2020-05-25 15:38:28 +0200

    init module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { IconsModule } from './icons/icons.module';
+import { SharedModule } from './shared/shared.module';
+import { TextModule } from './text/text.module';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    TextModule,
+    IconsModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
