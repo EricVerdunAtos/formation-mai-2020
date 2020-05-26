411b84d1c595f46e1e29e0c5190cb1279c40c5da
Eric Verdun <eric.verdun@atos.net>
2020-05-26 11:09:10 +0200

    add icons components

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { UiComponent } from './components/ui/ui.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent],
+  exports: [UiComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IconsModule
   ]
 })
 export class UiModule { }
