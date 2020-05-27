4010b55ceac7790e61c7e71b5961531d2afe1c40
Eric Verdun <eric.verdun@atos.net>
2020-05-27 11:20:24 +0200

    examples pipes angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { IconsModule } from './icons/icons.module';
+import { LoginModule } from './login/login.module';
 import { SharedModule } from './shared/shared.module';
 import { TextModule } from './text/text.module';
 import { UiModule } from './ui/ui.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LoginModule } from './login/login.module';
-import { HttpClientModule } from '@angular/common/http';
 
+// the second parameter 'fr-FR' is optional
+registerLocaleData(localeFr, 'fr-FR');
 
 @NgModule({
   declarations: [
@@ -30,7 +34,7 @@ import { HttpClientModule } from '@angular/common/http';
     FontAwesomeModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
