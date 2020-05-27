fbc9fecab78bca084c8d385be75b6a0f9f758bc4
Eric Verdun <eric.verdun@atos.net>
2020-05-27 14:49:47 +0200

    new encapsulation

---
+++ src/app/shared/directives/state.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, Input, HostBinding } from '@angular/core';
+import { StateOrder } from '../enums/state-order.enum';
+
+@Directive({
+  selector: '[appState]'
+})
+export class StateDirective {
+  @Input() appState: StateOrder;
+  @HostBinding('class') nomClass: string;
+  constructor() {
+
+  }
+
+  ngOnChanges() {
+    console.log(this.appState);
+    this.nomClass = this.formatClass(this.appState);
+  }
+
+  private formatClass(state: string): string {
+    return `state-${state.replace(/\s/g, '').toLowerCase()}`;
+  }
+
+}
+
+// appState vaut CANCEL => state-cancel
+// appState vaut OPTION => state-option
+// appState vaut CONFIRMED => state-confirmed
+// binder l'attribut class du host element td avec state-cancel, state-option ou state-confirmed
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
+import { StateDirective } from './directives/state.directive';
 
 
 
 @NgModule({
-  declarations: [BtnComponent, TableLightComponent, TotalPipe],
+  declarations: [BtnComponent, TableLightComponent, TotalPipe, StateDirective],
   imports: [
     CommonModule
   ],
-  exports: [BtnComponent, TableLightComponent, TotalPipe]
+  exports: [BtnComponent, TableLightComponent, TotalPipe, StateDirective]
 })
 export class SharedModule { }
