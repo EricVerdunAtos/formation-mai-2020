2fe0cd5bfd7654121314a65bf3571b6c1dbbcf5e
Eric Verdun <eric.verdun@atos.net>
2020-05-27 11:50:31 +0200

    table light dynamic with @Input() and ng-content

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -7,21 +7,11 @@ import { Order } from '../../models/order';
   styleUrls: ['./table-light.component.scss']
 })
 export class TableLightComponent implements OnInit {
-  @Input() collection: Order[];
   @Input() headers: string[];
   constructor() {
-    console.log(this.collection);
-  }
-
-  ngOnChanges(): void {
-    console.log(this.collection);
-
   }
 
   ngOnInit(): void {
-    console.log(this.collection);
-    console.log(this.headers);
-
   }
 
 }
