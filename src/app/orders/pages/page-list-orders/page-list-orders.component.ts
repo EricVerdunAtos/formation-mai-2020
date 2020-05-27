f1e8c321821971600defb109a522b757358d429b
Eric Verdun <eric.verdun@atos.net>
2020-05-27 16:46:43 +0200

    @Output()

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../../services/orders.service';
 import { Order } from 'src/app/shared/models/order';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
+import { Btn } from 'src/app/shared/interfaces/btn';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -11,10 +12,25 @@ import { StateOrder } from 'src/app/shared/enums/state-order.enum';
 export class PageListOrdersComponent implements OnInit {
   public collection: Order[];
   public headers: string[];
+  public btnRoute: Btn;
+  public btnHref: Btn;
+  public btnAction: Btn;
   public states = Object.values(StateOrder);
   constructor(private os: OrdersService) { }
 
   ngOnInit(): void {
+    this.btnRoute = {
+      label: "Add an order",
+      route: "add"
+    };
+    this.btnHref = {
+      label: "Go to Google",
+      href: "http://www.google.fr"
+    };
+    this.btnAction = {
+      label: 'Open dialogue',
+      action: true
+    };
     this.os.collection.subscribe((datas) => {
       // console.log(datas);
       this.collection = datas;
@@ -38,6 +54,11 @@ export class PageListOrdersComponent implements OnInit {
       item.state = res.state;
 
     });
+
+  }
+
+  public openPopUp() {
+    console.log("Open popup");
 
   }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BtnComponent } from './components/btn/btn.component';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
-import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TotalPipe } from './pipes/total.pipe';
 
 
 
 @NgModule({
-  declarations: [BtnComponent, TableLightComponent, TotalPipe, StateDirective],
+  declarations: [BtnComponent, TableLightComponent, TotalPipe, StateDirective, TableDarkComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
-  exports: [BtnComponent, TableLightComponent, TotalPipe, StateDirective]
+  exports: [BtnComponent, TableLightComponent, TotalPipe, StateDirective, TableDarkComponent]
 })
 export class SharedModule { }
