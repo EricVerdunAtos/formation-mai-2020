0a07fefa59641a22ddd3efc4ff5ba5c82e8601f7
Eric Verdun <eric.verdun@atos.net>
2020-05-27 11:41:46 +0200

    pipe custom

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
+import { TotalPipe } from './pipes/total.pipe';
 
 
 
 @NgModule({
-  declarations: [BtnComponent, TableLightComponent],
+  declarations: [BtnComponent, TableLightComponent, TotalPipe],
   imports: [
     CommonModule
   ],
-  exports: [BtnComponent, TableLightComponent]
+  exports: [BtnComponent, TableLightComponent, TotalPipe]
 })
 export class SharedModule { }
